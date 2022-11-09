6f030b6c24f44673363879ebd70b38c886d84433
Jeff <jeffersonorozco24@gmail.com>
2022-11-09 16:13:56 -0500

    Taller de repaso terminado

--- Ejercicios/src/ejercicios/Ejercicios.java
+++ Ejercicios/src/ejercicios/Ejercicios.java
@@ -53,7 +53,7 @@ public class Ejercicios {
 
                 }
 
-                break;
+                break;//Fin main condicionales
 
             case 2://Main bucles
 
@@ -71,84 +71,194 @@ public class Ejercicios {
 
                 }
 
-                break;
+                break;//Fin main bucles
 
             case 3: //Taller de repaso Modulo 3
+                boolean backtomain = true;
+                while (backtomain) {
+                    System.out.println("\nTaller de repaso Modulo 3\n\n"
+                            + "Selecciona una opcion:\n\n"
+                            + "1.Cree un método que dado un número entero positivo y mayor que 1\n"
+                            + "determine si es este un número perfecto o no.\n\n"
+                            + "2.Construya un programa que permita al usuario ingresar una palabra como si\n"
+                            + "fuese su contraseña, después de esto, el programa debe pedir de manera\n"
+                            + "consecutiva al usuario que ingrese nuevamente la contraseña hasta que esta\n"
+                            + "coincida con la primera palabra ingresada.\n\n"
+                            + "3.Construya un método que permita determinar si un número es primo, si es\n"
+                            + "así que calcule su tabla de multiplicar desde el 1 hasta el 20.\n\n"
+                            + "4.Construya un programa tipo menú que le permita realizar cualquiera de los\n"
+                            + "ejercicios del 1 al 3, una última opción que le permita dar por finalizada la\n"
+                            + "ejecución, si la opción elegida es diferente a cualquiera de estas le debe\n"
+                            + "solicitar nuevamente el ingreso de una opción del menú. (Siempre que\n"
+                            + "termine una de las tareas del menú, regresará a dar las opciones del menú\n"
+                            + "hasta que elija la opción salir).\n\n5.Para finalizar la ejecucion.");
 
-                System.out.println("\nTaller de repaso Modulo 3\n\n"
-                        + "Selecciona una opcion:\n\n"
-                        + "1.Cree un método que dado un número entero positivo y mayor que 1\n"
-                        + "determine si es este un número perfecto o no.\n\n"
-                        + "2.Construya un programa que permita al usuario ingresar una palabra como si\n"
-                        + "fuese su contraseña, después de esto, el programa debe pedir de manera\n"
-                        + "consecutiva al usuario que ingrese nuevamente la contraseña hasta que esta\n"
-                        + "coincida con la primera palabra ingresada.\n\n"
-                        + "3.Construya un método que permita determinar si un número es primo, si es\n"
-                        + "así que calcule su tabla de multiplicar desde el 1 hasta el 20.\n\n"
-                        + "4.Construya un programa tipo menú que le permita realizar cualquiera de los\n"
-                        + "ejercicios del 1 al 3, una última opción que le permita dar por finalizada la\n"
-                        + "ejecución, si la opción elegida es diferente a cualquiera de estas le debe\n"
-                        + "solicitar nuevamente el ingreso de una opción del menú. (Siempre que\n"
-                        + "termine una de las tareas del menú, regresará a dar las opciones del menú\n"
-                        + "hasta que elija la opción salir).");
-                int inputOption3_MainTallerDeRepasoModulo3 = scanner.nextInt();
+                    int inputOption3_MainTallerDeRepasoModulo3 = scanner.nextInt();
 
-                switch (inputOption3_MainTallerDeRepasoModulo3) {
+                    switch (inputOption3_MainTallerDeRepasoModulo3) {
 
-                    case 1:
+                        case 1:
 
-                        System.out.println("Has eligido la opcion 1\nPor favor digita un numero:");
-                        int inputNumber1_MainTallerDeRepasoModulo3Option1 = scanner.nextInt();
-                        String resultCase1_MainTallerDeRepasoModulo3 = validarNumeroPefecto(inputNumber1_MainTallerDeRepasoModulo3Option1);
-                        System.out.println(resultCase1_MainTallerDeRepasoModulo3);
-                        break;
+                            System.out.println("Has eligido la opcion 1\nPor favor digita un numero:");
+                            int inputNumber1_MainTallerDeRepasoModulo3Option1 = scanner.nextInt();
+                            String resultCase1_MainTallerDeRepasoModulo3 = validarNumeroPefecto(inputNumber1_MainTallerDeRepasoModulo3Option1);
+                            System.out.println(resultCase1_MainTallerDeRepasoModulo3);
 
-                    case 2:
-                        System.out.println("Has eligido la opcion 1\nPor favor crea una contraseña:");
-                        String inputPassword_MainTallerDeRepasoModulo3Option2 = scanner.next();
-                        System.out.println("Contraseña creada satisfactoriamente ;D\n"
-                                + "Verifica tu contraseña:");
-                        String inputVerificPassword_MainTallerDeRepasoModulo3Option2 = scanner.next();
-                        boolean resultValidation = inputPassword_MainTallerDeRepasoModulo3Option2 == inputVerificPassword_MainTallerDeRepasoModulo3Option2;
+                            boolean finalizacion = true;
+                            while (finalizacion) {
+                                System.out.println("Digite 0 para regresar al menu\nDigite 1 para terminar la ejecucioon");
+                                int back = scanner.nextInt();
+                                switch (back) {//De finalizacion
 
-                        if (inputPassword_MainTallerDeRepasoModulo3Option2 == inputVerificPassword_MainTallerDeRepasoModulo3Option2) {
+                                    case 0://repetir menu
+                                        finalizacion = false;
+                                        backtomain = true;
+                                        break;
 
-                            resultValidation = true;
-                            System.out.println("Contraseña valida :D");
+                                    case 1://Terminar ejecucion
+                                        System.out.println("Ejecucion terminada, regresa pronto :)");
+                                        finalizacion = false;
+                                        backtomain = false;
+                                        break;
 
-                        }
+                                    default://Repetir finalizacion
 
-                            
-                            while (resultValidation = false) {
-                                System.out.println("Contraseña invalida D:\n"
-                                        + "Digite nuevamente su contraseña:");
-                                inputVerificPassword_MainTallerDeRepasoModulo3Option2 = scanner.next();
-                                if (inputPassword_MainTallerDeRepasoModulo3Option2 == inputVerificPassword_MainTallerDeRepasoModulo3Option2) {
-                                    resultValidation = false;
-                                }
-
+                                        System.out.println("Numero invalido >:c");
+                                        finalizacion = true;
+                                        break;
+                                }//Fin finalizacion
                             }
-                        
 
-                        break;
+                            break;
+                        case 2:
+                            System.out.println("Has eligido la opcion 2\nPor favor crea una contraseña:");
+                            String inputPassword_MainTallerDeRepasoModulo3Option2 = scanner.next();
+                            System.out.println("Contraseña creada satisfactoriamente ;D\n"
+                                    + "Verifica tu contraseña:");
+                            String inputVerificPassword_MainTallerDeRepasoModulo3Option2 = scanner.next();
+                            boolean resultValidation;
 
-                    case 3:
-                        System.out.println("Has eligido la opcion 1\nDigite un numero primo para saber su tabla del 20:");
-                        int inputNumberPrimo_MainTallerDeRepasoModulo3Option3 = scanner.nextInt();
-                        multiplicarx20_NumeroPrimo(inputNumberPrimo_MainTallerDeRepasoModulo3Option3);
-                        
-                        
-                        
+                            if (inputPassword_MainTallerDeRepasoModulo3Option2.equals(inputVerificPassword_MainTallerDeRepasoModulo3Option2)) {
 
-                        break;
+                                resultValidation = false;
+                                System.out.println("Contraseña valida :D");
 
-                    default:
+                            } else {
 
-                        break;
+                                resultValidation = true;
+                                while (resultValidation) {
+                                    System.out.println("Contraseña invalida D:\n"
+                                            + "Digite nuevamente su contraseña:");
+                                    inputVerificPassword_MainTallerDeRepasoModulo3Option2 = scanner.next();
+                                    if (inputPassword_MainTallerDeRepasoModulo3Option2.equals(inputVerificPassword_MainTallerDeRepasoModulo3Option2)) {
+                                        resultValidation = false;
+                                        System.out.println("Contraseña valida :D\nEjecucion finalizada :)");
+                                    }
 
+                                }
+                            }
+                            finalizacion = true;
+                            while (finalizacion) {
+                                System.out.println("Digite 0 para regresar al menu\nDigite 1 para terminar la ejecucioon");
+                                int back = scanner.nextInt();
+                                switch (back) {//De finalizacion
+
+                                    case 0://repetir menu
+                                        finalizacion = false;
+                                        backtomain = true;
+                                        break;
+
+                                    case 1://Terminar ejecucion
+                                        System.out.println("Ejecucion terminada, regresa pronto :)");
+                                        finalizacion = false;
+                                        backtomain = false;
+                                        break;
+
+                                    default://Repetir finalizacion
+
+                                        System.out.println("Numero invalido >:c");
+                                        finalizacion = true;
+                                        break;
+                                }//Fin finalizacion
+                            }
+
+                            break;
+
+                        case 3:
+                            System.out.println("Has eligido la opcion 3\nDigite un numero primo para saber su tabla del 20:");
+                            int inputNumberPrimo_MainTallerDeRepasoModulo3Option3 = scanner.nextInt();
+                            multiplicarx20_NumeroPrimo(inputNumberPrimo_MainTallerDeRepasoModulo3Option3);
+
+                            finalizacion = true;
+                            while (finalizacion) {
+                                System.out.println("Digite 0 para regresar al menu\nDigite 1 para terminar la ejecucioon");
+                                int back = scanner.nextInt();
+                                switch (back) {//De finalizacion
+
+                                    case 0://repetir menu
+                                        finalizacion = false;
+                                        backtomain = true;
+                                        break;
+
+                                    case 1://Terminar ejecucion
+                                        System.out.println("Ejecucion terminada, regresa pronto :)");
+                                        finalizacion = false;
+                                        backtomain = false;
+                                        break;
+
+                                    default://Repetir finalizacion
+
+                                        System.out.println("Numero invalido >:c");
+                                        finalizacion = true;
+                                        break;
+                                }//Fin finalizacion
+                            }
+                            break;
+
+                        case 4:
+                            System.out.println("Has eligido la opcion 4\nEl punto 4 ya esta codificado ;)");
+                            finalizacion = true;
+                            while (finalizacion) {
+                                System.out.println("Digite 0 para regresar al menu\nDigite 1 para terminar la ejecucioon");
+                                int back = scanner.nextInt();
+                                switch (back) {//De finalizacion
+
+                                    case 0://repetir menu
+                                        finalizacion = false;
+                                        backtomain = true;
+                                        break;
+
+                                    case 1://Terminar ejecucion
+                                        System.out.println("Ejecucion terminada, regresa pronto :)");
+                                        finalizacion = false;
+                                        backtomain = false;
+                                        break;
+
+                                    default://Repetir finalizacion
+
+                                        System.out.println("Numero invalido >:c");
+                                        finalizacion = true;
+                                        break;
+                                }//Fin finalizacion
+                            }
+
+                            break;
+
+                        case 5:
+                            System.out.println("Ejecucion terminada, regresa pronto :)");
+                            backtomain = false;
+                            break;
+
+                        default:
+
+                            System.out.println("Numero fuera de rango x)");
+                            backtomain = true;
+                            break;
+
+                    }
                 }
 
-                break;
+                break;//Fin taller de repaso modulo 3
 
         }
     }
@@ -182,19 +292,18 @@ public class Ejercicios {
     }
 
     static void multiplicarx20_NumeroPrimo(int number1) {//multiplicar numero primo
-        
 
         if (number1 % 2 == 0) {
 
             for (int i = 1; i <= 20; i++) {
 
                 int result = number1 * i;
-                System.out.println(number1+"x"+i+"="+result);
+                System.out.println(number1 + "x" + i + "=" + result);
             }
 
-        }else{
-        
-        System.out.println("No es numero primo");
+        } else {
+
+            System.out.println("No es numero primo");
         }
 
     }
